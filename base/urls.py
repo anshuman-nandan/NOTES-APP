ee2a8c768114beb011a7670268bd7be6d05ddd2c
anshumannandan <anshuman21xxx034@akgec.ac.in>
2022-10-21 08:19:12 +0530

    modify notes

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.homepage, name='home'),
     path('notespage/<str:pk>/', views.notespage, name='notes'),
     path('newnote/', views.newnote, name='newnote'),
+    path('modifiednote/<str:pk>/', views.modifiednote, name='modifiednote'),
 ]
--- base/views.py
+++ base/views.py
@@ -19,12 +19,22 @@ def notespage(request, pk):
     return render(request, 'base/notespage.html', dict)
 
 
-# Create your views here.
-
 def newnote(request):
     form = notesform
     if request.method == 'POST':
         form = notesform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    dict = {'form': form}
+    return render(request, 'base/notesform.html', dict)
+
+
+def modifiednote(request, pk):
+    note = Notes.objects.get(id=pk)
+    form = notesform(instance=note)
+    if request.method == 'POST':
+        form = notesform(request.POST, instance=note)
         if form.is_valid():
             form.save()
             return redirect('home')
